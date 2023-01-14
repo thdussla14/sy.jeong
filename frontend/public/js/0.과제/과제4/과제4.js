752030f66fe4f701a06b5b92a79eab6724548b72
judy1 <judy1@DESKTOP-soso>
2023-01-14 20:46:10 +0900

    js과제_도서관리시스템(관리자)

--- frontend/public/js/0.과제/과제4/과제4.js
+++ frontend/public/js/0.과제/과제4/과제4.js
@@ -50,20 +50,79 @@
 
  console.log('실행확인') //연동확인용
  
- let 도서목록 = ['혼자공부하는자바', '이것이자바다','열혈 C언어']
- let 대여목록 = []
+ let 도서목록 = ['혼자공부하는자바','이것이자바다','열혈 C언어']
+ let 대여목록 = ['혼자공부하는자바']
  
- let html = `<tr><th>번호</th><th>도서명</th><th>도서대여여부</th><th>삭제</th></tr>`
+ //-------------------230114 정소연 작성----------------------------//
 
- for (let i=1 ; i<= 도서목록.length ; i++)
- {html += `<tr>
- 				<td>${i+1}</td>
- 				<td> ${도서목록[i]}</td>
- 				<td>도서대여여부</td>
- 				<td><button onclick="onDelete()" > 삭제 </button></td>
- 			</tr>`}
- 
- document.querySelector('.bookmaster').innerHTML = html
- 
+ // 도서현황 출력
+ function printcontent() 
+ 	// 테이블틀 선언
+  	{let html = `<tr class="title">	
+						<th>번호		</th>
+						<th>도서		</th>
+						<th>도서대여여부</th>
+						<th>비고		</th>
+				</tr>`					
+	for (let i=0 ; i< 도서목록.length ; i++)
+	// 대여여부 확인
+ 		{if(check(도서목록[i])==0)
+			{html +=
+	 			 `<tr>
+	 			<td> ${i+1}	</td>
+	 			<td> ${도서목록[i]}</td>
+	 			<td> 
+	 				<button> 대여중 </button>
+	 			</td>
+	 			<td></td>
+	 			</tr>`}
+ 		else{html +=
+	 			 `<tr>
+	 			<td> ${i+1}	</td>
+	 			<td> ${도서목록[i]}</td>
+	 			<td> 
+	 				<button> 대여가능 </button>
+	 			</td>
+	 			<td><button onclick="onDelete()" > 삭제 </button></td>
+	 			</tr>`}
+ 			
+ 	   	document.querySelector('.bookmaster').innerHTML = html}
+ 	}// function end
+
+// 대여여부 확인
+
+function check(x)
+	// 대여목록에 포함되어있는가?
+   { if(대여목록.indexOf(x)>=0)
+		// 포함되어있음  	= 대여중
+   		{return 0;}					
+		// 포함되어있지 않음 = 대여가능
+   	else{return 1;}
+   	}// function end				
+
+ // 도서등록버튼 클릭시 신규도서 도서목록에 추가
+ function onAdd()
+ 	{// 입력된 도서명 불러오기
+	let newbook = document.querySelector('.newbook').value	
+ 	console.log(newbook)
+ 	// 기존 도서목록과 비교하여 중복확인	 	
+ 	if(도서목록.indexOf(newbook)>=0)
+ 		{alert('이미 등록된 도서입니다.')}
+ 	// 도서명 글자 수 제한 
+	else if(newbook.length<5||newbook.length>10)
+		{alert('5 ~ 10자 이내로 입력해주세요.')}
+	// 새로운 도서를 도서목록에 추가
+	else{도서목록.push(newbook);}
+	printcontent() 
+	console.log(도서목록)
+ 	}// function end
+ 	
+ // 삭제버튼 클릭시 도서목록에서 해당 도서 제거	 //수정 필요함
+ function onDelete(dno) 
+ 	{도서목록.splice(dno,1)
+ 	console.log(도서목록)	 	
+	printcontent()
+	}// function end
+
  
  
