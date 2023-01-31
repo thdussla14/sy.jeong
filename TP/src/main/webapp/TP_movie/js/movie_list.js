ee7a2881fb1f5c405ae002f3554f0b09de6dd7a9
judy1 <judy1@DESKTOP-soso>
2023-01-31 23:03:02 +0900

    20230131

---
+++ TP/src/main/webapp/TP_movie/js/movie_list.js
@@ -0,0 +1,35 @@
+
+let movieList=[
+	{name:'BTS: Yet To Come in Cinemas',img:'방탄콘서트.jpg',감독 :'오윤동' 	   , 장르 : '공연실황', 	평점:8.83, 개봉일:'20230201'},
+	{name:'아바타-물의 길',		img:'아바타.jpg',			감독 :'James Cameron'   , 장르 : 'SF', 		평점:8.83, 개봉일:'20221214'},
+	{name:'오늘 밤,세계에서 이 사랑이 사라진다해도',	img:'오늘밤.jpg',	감독 :'Takahiro Miki' , 장르 : 'SF', 	평점:8.83, 개봉일:'20221214'},
+	{name:'영웅',				img:'영웅.jpg',			감독 :'윤제균' 		   , 장르 : '드라마', 		평점:8.42, 개봉일:'20221221'},
+	{name:'교섭',				img:'교섭.jpg',			감독 :'임순례' 		   , 장르 : '드라마', 		평점:6.29, 개봉일:'20230118'},
+	{name:'상견니',			img:'상견니.jpg',			감독 :'Tianren Huang'   , 장르 : '드라마', 		평점:7.85, 개봉일:'20230125'},
+	{name:'올뺴미',			img:'올빼미.jpg',			감독 :'안태진' 		   , 장르 : '스릴러', 		평점:8.73, 개봉일:'20221113'},
+	{name:'메간',				img:'메간.jpg',			감독 :'Gerard Johnstone', 장르 : '공포', 		평점:7.34, 개봉일:'20230125'},
+	{name:'슬램덩크',			img:'슬램덩크.jpg',		감독 :'Takehiko Inoue', 장르 : '애니메이션',		평점:9.24, 개봉일:'20230104'},
+	{name:'돌핀보이',			img:'돌핀보이.jpg',		감독 :'Andrey Goglev' , 장르 : '애니메이션', 		평점:6.00, 개봉일:'20230125'},
+	{name:'몬스터하우스',		img:'몬스터하우스.jpg',		감독 :'James Cameron' , 장르 : 'SF', 			평점:8.83, 개봉일:'20221214'},
+	{name:'장화신은 고양이:끝내주는 모험',img:'장화신은고양이.jpg',	감독 :'Joel Crawford' , 장르 : '애니메이션',	평점:8.41, 개봉일:'20230104'},	
+]
+
+movie_print()
+
+function movie_print(){
+	
+	let html = ``
+	
+	movieList.forEach((o,i)=>{
+		
+		html += `<div class="item" >
+					<img class="m_img" src="img/${o.img}" >
+					<div class="m_name"> ${o.name}	</div>
+					<div class="m_info"> 평점  <span class="percent"> ${o.평점}	</span> </div>
+					<div class="m_date"> ${o.개봉일} 개봉 	</div>
+					<div > <a href="movie_view.html"> <button class="viewbtn" type="button"> 상세보기 </button> </a> </div>
+				</div>`
+		})
+		
+	document.querySelector('.items').innerHTML = html
+}
