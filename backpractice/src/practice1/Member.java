b5946d9aa2494a03bc7c28b8bed111a8330ce850
judy1 <judy1@DESKTOP-soso>
2023-02-09 22:51:12 +0900

    23-02-09 도서시스템_JAVA

---
+++ backpractice/src/practice1/Member.java
@@ -0,0 +1,20 @@
+package practice1;
+
+import java.util.ArrayList;
+
+
+public class Member {
+	
+	// 1. 필드
+	String id;
+	String password;
+	String name; 
+	String phone; 
+	ArrayList<Book> rentList = new ArrayList<>();
+	
+	// 2. 생성자
+	
+	// 3. 메소드
+	
+}
+
